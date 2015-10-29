8b60f002a672288a911b62e7de4112c38d1c9070
tsszh <songzhpku@gmail.com>
2015-10-28 21:43:33 -0500

    Build The Demo Page

--- js/password-gen.js
+++ js/password-gen.js
@@ -1,2 +1,15 @@
-/* global */
+/* global md5, console*/
 'use strict';
+
+var PassGen = (function(){
+	return {
+		getPass : function( key, length, mode ){
+			if ( !length ) length = 16;
+			if ( !mode ) mode = 'complex';
+			return md5(key);
+		}
+	};
+})();
+
+if ( !PassGen )
+	console.log('Cannot Create PassGen!');
